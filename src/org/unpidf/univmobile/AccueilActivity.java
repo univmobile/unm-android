fa988cb63570c91874a33f62f94633e71676702f
mgauzins <michel.gauzins@gmail.com>
2014-07-15 14:47:10 +0200

    Init

--- src/org/unpidf/univmobile/AccueilActivity.java
+++ src/org/unpidf/univmobile/AccueilActivity.java
@@ -1,9 +1,14 @@
 package org.unpidf.univmobile;
 
+import org.unpidf.univmobile.manager.DataManager;
+import org.unpidf.univmobile.view.SelectUniversityActivity;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class AccueilActivity extends Activity {
 
@@ -11,6 +16,30 @@ public class AccueilActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_accueil);
+		initEvents();
+	}
+
+	private void initData() {
+		if(DataManager.getCurrentUniversity() == null){
+			((TextView)findViewById(R.id.universitySelected)).setText("Aucune université sélectionée.");
+		}else{
+			((TextView)findViewById(R.id.universitySelected)).setText(DataManager.getCurrentUniversity().getName());
+		}
+	}
+
+	private void initEvents() {
+		findViewById(R.id.selectUniversity).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getApplicationContext(), SelectUniversityActivity.class));
+			}
+		});
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		initData();
 	}
 
 }
--- src/org/unpidf/univmobile/manager/DataManager.java
+++ src/org/unpidf/univmobile/manager/DataManager.java
@@ -1,14 +1,21 @@
 package org.unpidf.univmobile.manager;
 
+import org.unpidf.univmobile.dao.University;
+
 public class DataManager {
 
 	private static DataManager instance;
-
+	private static University currentUniversity;
+	
 	public static DataManager getInstance(){
 		if(instance == null){
 			instance = new DataManager();
 		}
 		return instance;
+	}
+	
+	public static University getCurrentUniversity() {
+		return currentUniversity;
 	}
 
 }
