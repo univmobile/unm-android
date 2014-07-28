6754da76921e934809d67fe1d0fcd6e2141ac677
mgauzins <michel.gauzins@gmail.com>
2014-07-28 14:17:19 +0200

    - Change enter point to https://univmobile-dev.univ-paris1.fr/json/regions

--- UnivMobile/src/org/unpidf/univmobile/manager/MappingManager.java
+++ UnivMobile/src/org/unpidf/univmobile/manager/MappingManager.java
@@ -4,6 +4,6 @@ public class MappingManager {
 	
 	public static final String DIR_DATA = "/Android/data/org.unpidf.univmobile";
 	
-	private static final String URL = "http://univmobile.vswip.com/unm-backend-mock/";
+	private static final String URL = "https://univmobile-dev.univ-paris1.fr/json/";
 	public static final String URL_REGIONS = URL + "regions";
 }
