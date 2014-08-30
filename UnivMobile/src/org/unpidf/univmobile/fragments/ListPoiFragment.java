bda31d751769a11fda127b26aa261ca1f49cae83
David <david.andriana@avantage-compris.com>
2014-08-30 11:05:03 +0200

    Clean up: Code reformat from Eclipse, with UnivMobile prefs

--- UnivMobile/src/org/unpidf/univmobile/fragments/ListPoiFragment.java
+++ UnivMobile/src/org/unpidf/univmobile/fragments/ListPoiFragment.java
@@ -20,97 +20,101 @@ import android.view.ViewGroup;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.ExpandableListView.OnGroupClickListener;
+
 /**
  * List regions and universities. Fragment for: {@link GeocampusActivity}
+ * 
  * @author Michel
- *
  */
-public class ListPoiFragment extends Fragment{
+public class ListPoiFragment extends Fragment {
 
-	private ListPoiAdapter adapter;
-	
-	public static ListPoiFragment newInstance(String title) {
-		Bundle bundle = new Bundle();
-		bundle.putString("title", title);
-		ListPoiFragment frag = new ListPoiFragment();
-		frag.setArguments(bundle);
-		return frag;
-	}
+    private ListPoiAdapter adapter;
 
-	private BroadcastReceiver receiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			if(getView() == null || getActivity() == null){
-				return;
-			}
-			if(intent.getAction().equals(DataManager.NOTIF_POIS_OK)){
-				getView().findViewById(R.id.loader).setVisibility(View.GONE);
-				refreshData();
-			}else if(intent.getAction().equals(DataManager.NOTIF_POIS_ERR)){
-			}
-		}
-	}; 
+    public static ListPoiFragment newInstance(String title) {
+        final Bundle bundle = new Bundle();
+        bundle.putString("title", title);
+        ListPoiFragment frag = new ListPoiFragment();
+        frag.setArguments(bundle);
+        return frag;
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		adapter = null;
-		IntentFilter filter = new IntentFilter();
-		filter.addAction(DataManager.NOTIF_POIS_OK);
-		filter.addAction(DataManager.NOTIF_POIS_ERR);
-		LocalBroadcastManager.getInstance(getActivity()).registerReceiver(receiver, filter);
-		return inflater.inflate(R.layout.frag_list, container, false);
-	}
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (getView() == null || getActivity() == null) {
+                return;
+            }
+            if (intent.getAction().equals(DataManager.NOTIF_POIS_OK)) {
+                getView().findViewById(R.id.loader).setVisibility(View.GONE);
+                refreshData();
+            } else if (intent.getAction().equals(DataManager.NOTIF_POIS_ERR)) {
+            }
+        }
+    };
 
-	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        adapter = null;
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(DataManager.NOTIF_POIS_OK);
+        filter.addAction(DataManager.NOTIF_POIS_ERR);
+        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(receiver, filter);
+        return inflater.inflate(R.layout.frag_list, container, false);
+    }
 
-	protected void refreshData() {
-		final AnimatedExpandableListView listView = (AnimatedExpandableListView) getView().findViewById(R.id.listView);
-		if(adapter != null){
-			adapter.setList(DataManager.getInstance(getActivity()).getListPois());
-		}else{
-			listView.setChildIndicator(null);
-			listView.setGroupIndicator(null);
-			adapter = new ListPoiAdapter(getActivity(), DataManager.getInstance(getActivity()).getListPois());
-			listView.setAdapter(adapter);
-			listView.setOnGroupClickListener(new OnGroupClickListener() {
-				@Override
-				public boolean onGroupClick(ExpandableListView parent, View v,
-						int groupPosition, long id) {
-					if (listView.isGroupExpanded(groupPosition)) {
-	                    listView.collapseGroupWithAnimation(groupPosition);
-	                } else {
-	                    listView.expandGroupWithAnimation(groupPosition);
-	                }
-					return true;
-				}
-			});
-			listView.setOnChildClickListener(new OnChildClickListener() {
-				@Override
-				public boolean onChildClick(ExpandableListView parent, View v,
-						int groupPosition, int childPosition, long id) {
-					startActivity(new Intent(getActivity(), UniversityActivity.class).putExtra("poi", adapter.getChild(groupPosition, childPosition)));
-					return false;
-				}
-			});
-		}
-		for (int i = 0; i < DataManager.getInstance(getActivity()).getListPois().size(); i++) {
-			listView.expandGroup(i);
-		}
-	}
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 
-	@Override
-	public void onDestroyView() {
-		super.onDestroyView();
-		if(getActivity() != null){
-			try{
-				LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(receiver);
-			}catch(IllegalArgumentException e){
-				e.printStackTrace();
-			}
-		}
-	}
+    protected void refreshData() {
+        final AnimatedExpandableListView listView = (AnimatedExpandableListView) getView()
+                .findViewById(R.id.listView);
+        if (adapter != null) {
+            adapter.setList(DataManager.getInstance(getActivity()).getListPois());
+        } else {
+            listView.setChildIndicator(null);
+            listView.setGroupIndicator(null);
+            adapter = new ListPoiAdapter(getActivity(), DataManager.getInstance(getActivity())
+                    .getListPois());
+            listView.setAdapter(adapter);
+            listView.setOnGroupClickListener(new OnGroupClickListener() {
+                @Override
+                public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition,
+                        long id) {
+                    if (listView.isGroupExpanded(groupPosition)) {
+                        listView.collapseGroupWithAnimation(groupPosition);
+                    } else {
+                        listView.expandGroupWithAnimation(groupPosition);
+                    }
+                    return true;
+                }
+            });
+            listView.setOnChildClickListener(new OnChildClickListener() {
+                @Override
+                public boolean onChildClick(ExpandableListView parent, View v, int groupPosition,
+                        int childPosition, long id) {
+                    startActivity(new Intent(getActivity(), UniversityActivity.class).putExtra(
+                            "poi", adapter.getChild(groupPosition, childPosition)));
+                    return false;
+                }
+            });
+        }
+        for (int i = 0; i < DataManager.getInstance(getActivity()).getListPois().size(); i++) {
+            listView.expandGroup(i);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (getActivity() != null) {
+            try {
+                LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(receiver);
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
